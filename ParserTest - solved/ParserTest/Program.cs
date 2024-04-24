082939c0cf7357db0c7c4e68418d5523c1ed5db1
Xue Khang <xuekhang91@gmail.com>
2024-04-24 16:07:38 -0500

    Initial commit

---
+++ ParserTest - solved/ParserTest/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParserTest
+{
+	class Program
+	{
+		private static readonly string[][] TEST_DATA = new string[][]
+		{
+			new []{ "", "" },
+			new []{ (string)null, "" },
+			new []{ "test Test", "-1,-1"},
+			new []{ "\t\r\ntest\t TeSt", "-1,-1"},
+			new []{ "!@#$test1__TeSt2%^&*", "2,11"},
+			new []{ "()^%$test1__TeSt2--&*tes3t", "2,11"},
+			new []{ "!@#$1234test__TeSt++^-*tes3t==", "-1"},
+		};
+
+		private static readonly TokenAction[] TOKEN_ACTIONS = new[]
+		{
+			new TokenAction{ Name = "test", Code = -1},
+			new TokenAction{ Name = "test1", Code = 2},
+			new TokenAction{ Name = "test2", Code = 11},
+			new TokenAction{ Name = "test3", Code = 44}
+		};
+
+
+		static void Main(string[] args)
+		{
+			// Problem:
+			// Update StringParser Parse() method so it would return list of actions matching specified tokens from the supplied input string.
+			// Each token should be continuous sequence of alphanumeric characters.
+			// The sample TEST_DATA are given above, please feel free to extend it if neccessary.
+			// The current implementation passes 2 first cases but fails the rest.
+			// Please ensure the implementation is production quality- simple, easy to maintain, doesn't allocate unnecessary memory and has performance O(n) where n- length of the input string. The current implementation does not meet these requirements.
+			// Please do not use Regex to parse input string.
+
+			StringParser parser = new StringParser();
+
+			for (int i = 0; i < TEST_DATA.Length; i++)
+			{
+				string[] data = TEST_DATA[i];
+
+				string s = data[0];
+
+				try
+				{
+					IEnumerable<TokenAction> lst = parser.Parse(s, TOKEN_ACTIONS);
+
+					if (data[1] == string.Join(",", lst.Select(t => t.Code.ToString())))
+					{
+						Console.WriteLine($"{i}: passed ...");
+					}
+					else
+					{
+						Console.WriteLine($"{i}: not passed ...");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"{i}: error: '{ex.Message}'");
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+
+	class TokenAction
+	{
+		public string Name { get; set; }
+
+		public int Code { get; set; }
+	}
+}
---
+++ ParserTest - solved/ParserTest/StringParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParserTest
+{
+    class StringParser
+    {
+        public IEnumerable<TokenAction> Parse(string input, TokenAction[] tokenActions)
+        {
+            List<TokenAction> result = new List<TokenAction>();
+
+            int start = -1; // Start index of current token
+            int length = 0; // Length of current token
+            if (input == null) return Enumerable.Empty<TokenAction>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (char.IsLetterOrDigit(c))
+                {
+                    if (start == -1)
+                    {
+                        start = i; // Start a new token
+                        length = 1;
+                    }
+                    else
+                    {
+                        length++; // Continue current token
+                    }
+                }
+                else if (start != -1) // End of token
+                {
+                    string token = input.Substring(start, length);
+                    TokenAction action = tokenActions.FirstOrDefault(t => t.Name.Equals(token, StringComparison.OrdinalIgnoreCase));
+                    if (action != null)
+                    {
+                        result.Add(action);
+                    }
+                    start = -1; // Reset start index
+                }
+            }
+
+            // Check for token at the end of the input
+            if (start != -1)
+            {
+                string token = input.Substring(start, length);
+                TokenAction action = tokenActions.FirstOrDefault(t => t.Name.Equals(token, StringComparison.OrdinalIgnoreCase));
+                if (action != null)
+                {
+                    result.Add(action);
+                }
+            }
+
+            return result;
+        }
+    }
+}
